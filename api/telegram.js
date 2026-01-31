151b2d7839f83f3f321847f39c0855deabf6a3df
Sonic1300 <thefrench3925@outlook.fr>
2026-01-31 20:37:20 +0100

    telegram.js

--- api/telegram.js
+++ api/telegram.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+    return res.status(200).send("OK");
   }
 
   const { product, price, delivery, city, payment } = req.body;
